6c14d2f7993664d156c340a3390f46e748f690e1
Dennis Martin Cual <denniscual0716@gmail.com>
2025-05-05 13:10:04 +0800

    add force field in version object to determine if the commit is forceable or not

--- version-rspack-plugin.ts
+++ version-rspack-plugin.ts
@@ -5,46 +5,50 @@ import type { Compiler } from "@rspack/core";
 const PLUGIN_NAME = "VersionRspackPlugin";
 
 export class VersionRspackPlugin {
-	private versionFileName: string;
-	private buildDateElementId: string;
-	private buildNumberElementId: string;
+  private versionFileName: string;
+  private buildDateElementId: string;
+  private buildNumberElementId: string;
 
-	constructor() {
-		this.versionFileName = "version.json";
-		this.buildDateElementId = "cp_build_date";
-		this.buildNumberElementId = "cp_build_number";
-	}
+  constructor() {
+    this.versionFileName = "version.json";
+    this.buildDateElementId = "cp_build_date";
+    this.buildNumberElementId = "cp_build_number";
+  }
 
-	apply(compiler: Compiler) {
-		const buildDate = new Date().toISOString();
-		const buildNumber = process.env.REACT_APP__BUILD_NUMBER;
-		const commitHash = process.env.REACT_APP__COMMIT_HASH;
-		const commitMessage = process.env.REACT_APP__COMMIT_MESSAGE;
+  apply(compiler: Compiler) {
+    const buildDate = new Date().toISOString();
+    const buildNumber = process.env.REACT_APP__BUILD_NUMBER;
+    const commitHash = process.env.REACT_APP__COMMIT_HASH;
+    const commitMessage = process.env.REACT_APP__COMMIT_MESSAGE;
 
-		// Write version.json file
-		compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
-			const buildDateStaleThreshold = process.env
-				.REACT_APP_BUILD_DATE_STALE_THRESHOLD
-				? parseInt(process.env.REACT_APP_BUILD_DATE_STALE_THRESHOLD, 10)
-				: 1000 * 60 * 60 * 24 * 14; // Default to 2 weeks.
+    // Write version.json file
+    compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
+      const buildDateStaleThreshold = process.env
+        .REACT_APP_BUILD_DATE_STALE_THRESHOLD
+        ? parseInt(process.env.REACT_APP_BUILD_DATE_STALE_THRESHOLD, 10)
+        : 1000 * 60 * 60 * 24 * 14; // Default to 2 weeks.
 
-			const versionInfo = {
-				buildDate,
-				buildNumber,
-				/**
-				 * The minimum time difference (in milliseconds) between the current time and
-				 * the last update check that must have passed to trigger a new update check.
-				 */
-				buildDateStaleThreshold,
-				commitHash,
-				commitMessage,
-			};
+      const versionInfo = {
+        buildDate,
+        buildNumber,
+        /**
+         * The minimum time difference (in milliseconds) between the current time and
+         * the last update check that must have passed to trigger a new update check.
+         */
+        buildDateStaleThreshold,
+        force: isGitCommitAForceUpdate(commitMessage),
+      };
 
-			console.log("Logging version...");
+      console.log("Logging version...");
 
-			console.log({ versionInfo });
+      console.log({ versionInfo });
 
-			callback();
-		});
-	}
+      callback();
+    });
+  }
+}
+
+function isGitCommitAForceUpdate(commitMsg: string) {
+  const forceUpdateChars = "force-update!";
+  return new RegExp(forceUpdateChars, "i").test(commitMsg);
 }
