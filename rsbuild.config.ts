f6f6e0c6aec5d7bf7b748afe44d6e62b40a5623e
Dennis Martin Cual <denniscual0716@gmail.com>
2025-05-05 10:25:26 +0800

    expose env variables started with REACT_APP prefix

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "@rsbuild/core";
+import { defineConfig, loadEnv } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { VersionRspackPlugin } from "./version-rspack-plugin";
 
+const { publicVars } = loadEnv({ prefixes: ["REACT_APP_"] });
+
 export default defineConfig({
+	source: {
+		define: publicVars,
+	},
 	plugins: [pluginReact()],
 	tools: {
 		rspack(_, { appendPlugins }) {
