1d54348af7e9648d1ae5e5d20b55d55589af17f8
Dennis Martin Cual <denniscual0716@gmail.com>
2025-05-07 20:31:25 +0800

    remove prefix (#20)

--- version-rspack-plugin.ts
+++ version-rspack-plugin.ts
@@ -5,61 +5,61 @@ import type { Compiler } from "@rspack/core";
 const PLUGIN_NAME = "VersionRspackPlugin";
 
 export class VersionRspackPlugin {
-  private versionFileName: string;
-  private buildDateElementId: string;
-  private buildNumberElementId: string;
+	private versionFileName: string;
+	private buildDateElementId: string;
+	private buildNumberElementId: string;
 
-  constructor() {
-    this.versionFileName = "version.json";
-    this.buildDateElementId = "cp_build_date";
-    this.buildNumberElementId = "cp_build_number";
-  }
+	constructor() {
+		this.versionFileName = "version.json";
+		this.buildDateElementId = "cp_build_date";
+		this.buildNumberElementId = "cp_build_number";
+	}
 
-  apply(compiler: Compiler) {
-    const buildDate = new Date().toISOString();
-    const buildNumber = process.env.REACT_APP__BUILD_NUMBER;
-    const commitMessage = process.env.REACT_APP__COMMIT_MESSAGE;
-    const pullRequests = process.env.REACT_APP__PULL_REQUESTS;
+	apply(compiler: Compiler) {
+		const buildDate = new Date().toISOString();
+		const buildNumber = process.env.REACT_APP__BUILD_NUMBER;
+		const commitMessage = process.env.COMMIT_MESSAGE;
+		const pullRequests = process.env.PULL_REQUESTS;
 
-    // Write version.json file
-    compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
-      const buildDateStaleThreshold = process.env
-        .REACT_APP_BUILD_DATE_STALE_THRESHOLD
-        ? parseInt(process.env.REACT_APP_BUILD_DATE_STALE_THRESHOLD, 10)
-        : 1000 * 60 * 60 * 24 * 14; // Default to 2 weeks.
+		// Write version.json file
+		compiler.hooks.emit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
+			const buildDateStaleThreshold = process.env
+				.REACT_APP_BUILD_DATE_STALE_THRESHOLD
+				? parseInt(process.env.REACT_APP_BUILD_DATE_STALE_THRESHOLD, 10)
+				: 1000 * 60 * 60 * 24 * 14; // Default to 2 weeks.
 
-      console.log({ commitMessage, pullRequests });
+			console.log({ commitMessage, pullRequests });
 
-      const versionInfo = {
-        buildDate,
-        buildNumber,
-        /**
-         * The minimum time difference (in milliseconds) between the current time and
-         * the last update check that must have passed to trigger a new update check.
-         */
-        buildDateStaleThreshold,
-        force: isDeploymentForceable({
-          pullRequests: JSON.parse(pullRequests ?? "[]"),
-          commitMessage,
-        }),
-      };
+			const versionInfo = {
+				buildDate,
+				buildNumber,
+				/**
+				 * The minimum time difference (in milliseconds) between the current time and
+				 * the last update check that must have passed to trigger a new update check.
+				 */
+				buildDateStaleThreshold,
+				force: isDeploymentForceable({
+					pullRequests: JSON.parse(pullRequests ?? "[]"),
+					commitMessage,
+				}),
+			};
 
-      console.log("Logging version...");
+			console.log("Logging version...");
 
-      console.log({ versionInfo });
+			console.log({ versionInfo });
 
-      callback();
-    });
-  }
+			callback();
+		});
+	}
 }
 
 function isDeploymentForceable({
-  pullRequests,
-  commitMessage,
+	pullRequests,
+	commitMessage,
 }: {
-  pullRequests: any[];
-  commitMessage: string;
+	pullRequests: any[];
+	commitMessage: string;
 }) {
-  const forceUpdateChars = "force-update!";
-  return new RegExp(forceUpdateChars, "i").test(commitMessage);
+	const forceUpdateChars = "force-update!";
+	return new RegExp(forceUpdateChars, "i").test(commitMessage);
 }
